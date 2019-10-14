6aab7636b0c65b561bb59f0b49b0d07d54eb16fe
Robert <Robert.Elias@live.com>
2019-10-14 16:53:37 -0700

    Function conversion completed

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -5,21 +5,40 @@
 // };
 // myFunction();
 
+let myFunction = function myFunction() {
+    console.log("Function was invoked!");
+}
+myFunction();
+
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+
+let anotherFunction = function anotherFunction(param) {
+    return param;
+};
+anotherFunction("Example");
+
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
 
+let add = function add(param1, param2) {
+    return param1 + param2;
+};
+add(1, 2);
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
-
+var subtract = function subtract(param1, param2) {
+    return param1 - param2;
+  };
+  subtract(1, 2);
 
 // Stretch
 
